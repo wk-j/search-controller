525e6a33970414d6ba37899aa94ad4825c125740
wk-j <somnuk.wk@gmail.com>
2019-08-06 20:20:22 +0700

    Implement SearchFile2

--- Controllers/SearchController.cs
+++ Controllers/SearchController.cs
@@ -4,31 +4,44 @@ using System;
 using System.Collections.Generic;
 using System.IO;
 using Microsoft.AspNetCore.Mvc;
+using System.Linq;
 
-namespace SearchFile.Controllers {
+namespace SearchFile.Controllers
+{
+    public class SearchOptions
+    {
+        public string Path { set; get; }
+        public string Pattern { set; get; }
+    }
+
     [Route("api/[controller]/[action]")]
-    public class SearchController: ControllerBase {
+    [ApiController]
+    public class SearchController : ControllerBase
+    {
+        [HttpPost]
+        public List<string> SearchFile2(SearchOptions options) =>
+            new DirectoryInfo(options.Path).GetFiles(options.Pattern).Select(x => x.FullName).ToList();
 
-        public string A(){
-            return "A";
-        }
-        public List<string> searchFile(string path,string pattern){
+        public List<string> searchFile(string path, string pattern)
+        {
             // Process the list of files found in the directory.
             List<string> result = new List<string>();
-            string [] fileEntries = Directory.GetFiles(path);
-            foreach(string fileName in fileEntries){
+            string[] fileEntries = Directory.GetFiles(path);
+            foreach (string fileName in fileEntries)
+            {
                 string[] words = pattern.Split('*');
                 words = words[1].Split('"');
                 int lenfile = path.Length;
-                string fnCheck = fileName.Substring(fileName.Length-(words[0].Length),words[0].Length);
-                if(words[0].Equals(fnCheck)){
+                string fnCheck = fileName.Substring(fileName.Length - (words[0].Length), words[0].Length);
+                if (words[0].Equals(fnCheck))
+                {
                     result.Add(fileName);
                 }
             }
             return result;
         }
 
-        
+
 
     }
 }
